a7fb9d37602be1ed417a6596b3d06d234ac9f1cc
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-31 01:10:30 +0600

    multer

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
+
+const uploader = multer({
+  dest: "uploads/",
+});
+
 const app = express();
 app.use(express.json());
 
@@ -7,13 +13,13 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 
-
-app.post("/file",(req,res)=>{
-    
-
-})
-
-
+app.post("/file", uploader.single("image"), async (req, res) => {
+  try {
+    res.status(200).json(req.file);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
